27b7a679d8f7771748700bd0293d0857575fbcca
William Raymond <william.c.raymond@protonmail.com>
2024-11-23 14:05:37 -0500

    Initial commit

---
+++ ghost-report-api/src/main/java/com/ghostreport/ghostreportapi/GhostReportApiApplication.java
@@ -0,0 +1,13 @@
+package com.ghostreport.ghostreportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostReportApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhostReportApiApplication.class, args);
+    }
+
+}
---
+++ ghost-report-api/src/test/java/com/ghostreport/ghostreportapi/GhostReportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostreport.ghostreportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostReportApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
